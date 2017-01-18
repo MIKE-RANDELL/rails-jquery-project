1f8e858196695bcc6e059331cd195bad8f48845c
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-01-18 16:08:25 -0500

    added company books, item counts w/ AR method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -34,10 +34,20 @@ class AccountsController < ApplicationController
     redirect_to user_account_path(@account.user_id)
   end
 
-  #def books
-  #end
+  def books
+    @items = Item.all
+    @book_printout_array = item_setter(@items)
+  end
 
   private
+
+  def item_setter(items)
+    current_book = []
+    items.each do |item|
+      current_book << "#{item.item_type}: " + OrderItem.item_totals(item).to_s
+    end
+    return current_book
+  end
 
   def find_account
     @account = Account.find_by(:user_id => params[:id])
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,6 @@ class OrderItem < ApplicationRecord
   belongs_to :item
 
   scope :order_items_with_pallets, -> { where('pallet_count > ?', 0) }
+
+  scope :item_totals, -> (item) {where('item_id == ?', item.id).sum('quantity')}
 end
